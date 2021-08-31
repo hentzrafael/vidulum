13c9d5722190fc9258c51c267d0d3219c8e67716
Rafael Hentz <rafaelhentz172@gmail.com>
2021-08-31 18:37:14 -0300

    working default string on create user

--- api/resources/create_user.py
+++ api/resources/create_user.py
@@ -5,7 +5,7 @@ import sqlite3
 
 
 user = User()
-
+DEFAULTSTRING = "Não há informações cadastradas neste tópico"
 
 class __CreateUser(Resource):
     def post(self):
@@ -18,21 +18,20 @@ class __CreateUser(Resource):
                 photoURL=request.json['photoURL'],
                 idiomas=request.json['idiomas'],
                 
-                formacao_academica=request.json['formacao_academica'],
-                biografia=request.json['biografia'],
-                projetos=request.json['projetos'],
-                areas_de_atuacao=request.json['areas_de_atuacao'],
-                atuacao_profissional=request.json['atuacao_profissional'],
-                producao_bibliografica=request.json['producao_bibliografica'],
-                participacao_em_eventos=request.json['participacao_em_eventos'],
-                bancas_de_trabalho=request.json['bancas_de_trabalho'],
-                producao_tecnica=request.json['producao_tecnica'],
-                membro_do_corpo_editorial=request.json['membro_do_corpo_editorial'],
-                producao_artistica=request.json['producao_artistica'],
-                artigos=request.json['artigos'],
-                informacao_complementar=request.json['informacao_complementar'],
-                outras_informacoes_importantes=request.json['outras_informacoes_importantes'],
-
+                formacao_academica= None if 'formacao_academica' not in request.json else request.json['formacao_academica'],
+                biografia=None if 'biografia' not in request.json else request.json['biografia'],
+                projetos=None if 'projetos' not in request.json else request.json['projetos'],
+                areas_de_atuacao=None if 'areas_de_atuacao' not in request.json else request.json['areas_de_atuacao'],
+                atuacao_profissional=None if 'atuacao_profissional' not in request.json else request.json['atuacao_profissional'],
+                producao_bibliografica=None if 'producao_bibliografica' not in request.json else request.json['producao_bibliografica'],
+                participacao_em_eventos=None if 'participacao_em_eventos' not in request.json else request.json['participacao_em_eventos'],
+                bancas_de_trabalho=None if 'bancas_de_trabalho' not in request.json else request.json['bancas_de_trabalho'],
+                producao_tecnica=None if 'producao_tecnica' not in request.json else request.json['producao_tecnica'],
+                membro_do_corpo_editorial=None if 'membro_do_corpo_editorial' not in request.json else request.json['membro_do_corpo_editorial'],
+                producao_artistica=None if 'producao_artistica' not in request.json else request.json['producao_artistica'],
+                artigos=None if 'artigos' not in request.json else request.json['artigos'],
+                informacao_complementar=None if 'informacao_complementar' not in request.json else request.json['informacao_complementar'],
+                outras_informacoes_importantes=None if 'outras_informacoes_importantes' not in request.json else request.json['outras_informacoes_importantes']
             )
             return jsonify(
                 message="User created"
